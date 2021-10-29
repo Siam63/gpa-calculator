fe25ad0b365ba3b0cdcb8e196054c54b2f71c179
siamr <siam_1000@hotmail.com>
2021-10-28 23:21:52 -0400

    floating point decimal bug fixed, now works 100% correctly

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -9,40 +9,40 @@ function Form({ weightInput, setWeightInput, gradeInput, assessments, setAssessm
     const [gpa, setGpa] = useState(0);
 
     const calculateFinalGrade = () => {
-        if(parseInt(totalMark) >= 89.45){
+        if(parseFloat(totalMark) >= 89.45){
             setLetterGrade("A+");
             setGpa(4.33);
-        }else if(parseInt(totalMark) >= 83.45 && parseInt(totalMark) <= 89.44){
+        }else if(parseFloat(totalMark) >= 83.45 && parseFloat(totalMark) <= 89.44){
             setLetterGrade("A");
             setGpa(4.00);
-        }else if(parseInt(totalMark) >= 79.45 && parseInt(totalMark) <= 83.44){
+        }else if(parseFloat(totalMark) >= 79.45 && parseFloat(totalMark) <= 83.44){
             setLetterGrade("A-");
             setGpa(3.67);
-        }else if(parseInt(totalMark) >= 76.45 && parseInt(totalMark) <= 79.44){
+        }else if(parseFloat(totalMark) >= 76.45 && parseFloat(totalMark) <= 79.44){
             setLetterGrade("B+");
             setGpa(3.33);
-        }else if(parseInt(totalMark) >= 72.45 && parseInt(totalMark) <= 76.44){
+        }else if(parseFloat(totalMark) >= 72.45 && parseFloat(totalMark) <= 76.44){
             setLetterGrade("B");
             setGpa(3.00);
-        }else if(parseInt(totalMark) >= 69.45 && parseInt(totalMark) <= 72.44){
+        }else if(parseFloat(totalMark) >= 69.45 && parseFloat(totalMark) <= 72.44){
             setLetterGrade("B-");
             setGpa(2.67);
-        }else if(parseInt(totalMark) >= 66.45 && parseInt(totalMark) <= 69.44){
+        }else if(parseFloat(totalMark) >= 66.45 && parseFloat(totalMark) <= 69.44){
             setLetterGrade("C+");
             setGpa(2.33);
-        }else if(parseInt(totalMark) >= 62.45 && parseInt(totalMark) <= 66.44){
+        }else if(parseFloat(totalMark) >= 62.45 && parseFloat(totalMark) <= 66.44){
             setLetterGrade("C");
             setGpa(2.00);
-        }else if(parseInt(totalMark) >= 59.45 && parseInt(totalMark) <= 62.44){
+        }else if(parseFloat(totalMark) >= 59.45 && parseFloat(totalMark) <= 62.44){
             setLetterGrade("C-");
             setGpa(1.67);
-        }else if(parseInt(totalMark) >= 56.45 && parseInt(totalMark) <= 59.44){
+        }else if(parseFloat(totalMark) >= 56.45 && parseFloat(totalMark) <= 59.44){
             setLetterGrade("D+");
             setGpa(1.33);
-        }else if(parseInt(totalMark) >= 52.45 && parseInt(totalMark) <= 56.44){
+        }else if(parseFloat(totalMark) >= 52.45 && parseFloat(totalMark) <= 56.44){
             setLetterGrade("D");
             setGpa(1.00);
-        }else if(parseInt(totalMark) >= 49.45 && parseInt(totalMark) <= 52.44){
+        }else if(parseFloat(totalMark) >= 49.45 && parseFloat(totalMark) <= 52.44){
             setLetterGrade("D-");
             setGpa(0.67);
         }else{
@@ -70,10 +70,10 @@ function Form({ weightInput, setWeightInput, gradeInput, assessments, setAssessm
         ]);
         setGradeInput("");
         setWeightInput("");
-
+        
         setCount(count => count + 1);
-        setTotalWeight(totalWeight => totalWeight + parseInt(weightInput));
-        setTotalMark(totalMark => totalMark + ((parseInt(gradeInput) / 100) * parseInt(weightInput)));
+        setTotalWeight(totalWeight => totalWeight + parseFloat(weightInput));
+        setTotalMark(totalMark => parseFloat(totalMark + ((parseFloat(gradeInput) / 100) * parseFloat(weightInput))).toFixed(2));
     }
 
     return (
@@ -102,9 +102,9 @@ function Form({ weightInput, setWeightInput, gradeInput, assessments, setAssessm
                         <button className="form-button" onClick={resetFields}>Reset Fields</button>
                         <button className="form-button" onClick={calculateFinalGrade}>Calculate</button>
                     </div>
-                    <h2>Grade Received: {Math.round(totalMark)}%</h2>
-                    <h2>Current mark so far: {Math.round((totalMark / totalWeight) * 100)}%</h2>
-                    <h2>Total Weight of all Assessments: {Math.round(totalWeight)}%</h2>
+                    <h2>Grade Received: {parseFloat(totalMark).toFixed(2)}%</h2>
+                    <h2>Current mark so far: {parseFloat((totalMark / totalWeight) * 100).toFixed(2)}%</h2>
+                    <h2>Total Weight of all Assessments: {parseFloat(totalWeight).toFixed(2)}%</h2>
                     <h2>Grade: {letterGrade}</h2>
                     <h2>GPA: {gpa}</h2>
                     <h4 className="form-name">Developed by: MD Faiazur Rahman - 2021</h4>
